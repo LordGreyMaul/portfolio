f6afe5fdea5d4287f0535eb263da6c346f02499d
Graham morby Raybould <grahammorby-raybould@Grahams-iMac.home>
2016-05-29 10:03:28 +0100

    First commit
    Added base of site

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Graham Morby-Raybould | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link href='http://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <script src="js/vendor/modernizr.js"></script>
+</head>
+<body class="background1">
+<div class='fullscreenDiv'>
+    <div class="center"><p>Graham Morby-Raybould</p>
+        <p>Front End & PHP Web Developer</p>
+
+        <ul class="social">
+            <li><a href="https://www.facebook.com/tremonster82" target="_blank"><span class="fa fa-facebook-official"></span></a></li>
+            <li><a href="https://twitter.com/phoenixmorby" target="_blank"><span class="fa fa-twitter"></span></a></li>
+            <li><a href="https://plus.google.com/u/0/+GrahamMorbyRaybould/posts" target="_blank"><span class="fa fa-google-plus-square"></span></a></li>
+            <li><a href="https://www.linkedin.com/hp/?dnr=AyqzdkWjzUQ_xc4RAX_zqkm4GUJltcW05tDn" target="_blank"><span class="fa fa-linkedin-square"></span></a></li>
+        </ul>
+    </div>
+</div>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+    $(document).foundation();
+</script>
+
+</body>
+</html>
